d4374aa3f7924bf0a3a6daf3e55d3ec1be95c1e0
tiagopereira0601 <ferestiago06@gmail.com>
2025-10-02 20:05:38 -0300

    Add files via upload

---
+++ provah1/Controllers/Projeto.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.MicrosoftExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace provah1.Controllers
+{
+    public class Projeto : Controller
+    {
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [MinLength(3, ErrorMessage = "Nome deve ter no mínimo 3 caracteres")]
+        [MaxLength(50, ErrorMessage = "Nome deve ter no máximo 50 caracteres")]
+
+        public int Turma { get; set; }
+        [Required(ErrorMessage = "Número da turma é obrigatório")]
+        [(1,8, ErrorMessage = "O número tem que estar entre 1 e 8")]
+        public string Descricao { get; set; }
+        [MinLength(500, ErrorMessage = "Nome deve ter no máximo 500 caracteres")]
+
+        [Required(ErrorMessage = "Email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        public int NmroProjeto { get; set; }
+        [Required(ErrorMessage = "Número do projeto é obrigatório")]
+        [(10,99, ErrorMessage = "O número tem que estar entre 10 e 99")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ provah1/Controllers/ProjetoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace provah1.Controllers
+{
+    public class ProjetoController : Controller
+    {
+        private static List<Visita> visitaAlunos = new List<Visita>();
+
+        [ApiController]
+
+        public class Visita
+
+        {
+            [Required]
+            public string RAAluno { get; set; }
+            public DateTime DataVisita { get; set; }
+            public int NmroProjeto { get; set; }
+        }
+
+        [HttpPost]
+        [Route("Inserir")]
+        public IActionResult Inserir(Visita visita)
+        {
+            visitaAlunos.Add(visita);
+            return Ok($"Visita {visita} adicionada com sucesso");
+        }
+
+
+
+        [HttpGet]
+        [Route("Obter todas")]
+        public IActionResult ObterTodas(Visita visita)
+        {
+            return Ok(visita);
+
+        }
+    }
+}
+
